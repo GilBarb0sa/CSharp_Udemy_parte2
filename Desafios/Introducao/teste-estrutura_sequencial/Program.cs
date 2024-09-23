1912a3e459548003e0fc9aac6bed05934c9cbf4d
GilBarb0sa <gilbarbosafilho@gmail.com>
2024-09-23 12:07:34 -0300

    att

--- Desafios/Introducao/teste-estrutura_sequencial/Program.cs
+++ Desafios/Introducao/teste-estrutura_sequencial/Program.cs
@@ -9,35 +9,35 @@ namespace teste_estrutura_sequencial
 
             #region Exercício 1.1
 
-            //int codigo1, qtd1, codigo2, qtd2;
-            //float valor1, valor2, valorTotal;
+            int codigo1, qtd1, codigo2, qtd2;
+            float valor1, valor2, valorTotal;
 
-            //Console.WriteLine("Exercício 1.1:");
-            //Console.WriteLine("------------------------------------------------------------");
-            //Console.WriteLine("Entrada:");
-            //string entrada1 = Console.ReadLine();
-            //string[] partes1 = entrada1.Split(' ');//Divide a string por espaços
-            //codigo1 = int.Parse(partes1[0]); //Convert para int
-            //qtd1 = int.Parse(partes1[1]);
-            //valor1 = float.Parse(partes1[2]); //Convert para float
+            Console.WriteLine("Exercício 1.1:");
+            Console.WriteLine("------------------------------------------------------------");
+            Console.WriteLine("Entrada:");
+            string entrada1 = Console.ReadLine();
+            string[] partes1 = entrada1.Split(' ');//Divide a string por espaços
+            codigo1 = int.Parse(partes1[0]); //Convert para int
+            qtd1 = int.Parse(partes1[1]);
+            valor1 = float.Parse(partes1[2]); //Convert para float
 
-            //string entrada2 = Console.ReadLine();
-            //string[] partes2 = entrada2.Split(' ');
-            //codigo2 = int.Parse(partes2[0]);
-            //qtd2 = int.Parse(partes2[1]);
-            //valor2 = float.Parse(partes2[2]);
+            string entrada2 = Console.ReadLine();
+            string[] partes2 = entrada2.Split(' ');
+            codigo2 = int.Parse(partes2[0]);
+            qtd2 = int.Parse(partes2[1]);
+            valor2 = float.Parse(partes2[2]);
 
-            //Console.Clear();
+            Console.Clear();
 
-            //Console.WriteLine("Exercício 1.1:");
-            //Console.WriteLine("------------------------------------------------------------");
-            //Console.WriteLine("Entrada:");
-            //Console.WriteLine($"Código: {codigo1}, Quantidade: {qtd1}, Valor: R${valor1:F2}");
-            //Console.WriteLine($"Código: {codigo2}, Quantidade: {qtd2}, Valor: R${valor2:F2}");
-            //Console.WriteLine("------------------------------------------------------------");
+            Console.WriteLine("Exercício 1.1:");
+            Console.WriteLine("------------------------------------------------------------");
+            Console.WriteLine("Entrada:");
+            Console.WriteLine($"Código: {codigo1}, Quantidade: {qtd1}, Valor: R${valor1:F2}");
+            Console.WriteLine($"Código: {codigo2}, Quantidade: {qtd2}, Valor: R${valor2:F2}");
+            Console.WriteLine("------------------------------------------------------------");
 
-            //valorTotal = (qtd1 * valor1) + (qtd2 * valor2);
-            //Console.WriteLine($"Valor a pagar: R${valorTotal:F2}");
+            valorTotal = (qtd1 * valor1) + (qtd2 * valor2);
+            Console.WriteLine($"Valor a pagar: R${valorTotal:F2}");
 
             #endregion
 
