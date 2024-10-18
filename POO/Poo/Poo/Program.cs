45fade98c46767a5a7a6030a0c7e3fc1e57ef3af
GilBarb0sa <gilbarbosafilho@gmail.com>
2024-10-18 12:44:52 -0300

    estudos de poo

---
+++ POO/Poo/Poo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Globalization;
+
+namespace GerenciamentoDeEstoque
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto p = new Produto("TV", 500.00, 10);
+
+            p.SetNome("Tv 4k");
+
+            Console.WriteLine(p.GetNome());  //Minuto 11
+        }
+    }
+}
