3b766de3f4f701490fc429bad2c512e8ff2b888e
GilBarb0sa <gilbarbosafilho@gmail.com>
2024-10-22 10:04:05 -0300

    att do desafio

--- Desafios/Construtores this sobrecarga e encapsulamento/Banco/Banco/ContaBancaria.cs
+++ Desafios/Construtores this sobrecarga e encapsulamento/Banco/Banco/ContaBancaria.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Globalization;
 
 namespace Banco
@@ -16,7 +12,7 @@ namespace Banco
         public int NumeroConta(int conta)
         {
             Conta = conta;
-            return Conta;
+            return Conta; 
         }
 
         public void DepositoSemSaldo()
