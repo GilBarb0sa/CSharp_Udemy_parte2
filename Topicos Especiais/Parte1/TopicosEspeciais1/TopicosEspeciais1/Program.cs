b7541c252c82c9aa02524afe7e7dde15966c7791
GilBarb0sa <gilbarbosafilho@gmail.com>
2024-10-25 13:02:47 -0300

    topicos especiais parte 1

---
+++ Topicos Especiais/Parte1/TopicosEspeciais1/TopicosEspeciais1/Program.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Globalization;
+
+namespace TopicosEspeciais1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Switch Case
+            /*int x = int.Parse(Console.ReadLine());
+            string day;
+
+            switch (x)
+            {
+                case 1:
+                    day = "Sunday";
+                    break;
+                case 2:
+                    day = "Monday";
+                    break;
+                case 3:
+                    day = "Tuesday";
+                    break;
+                case 4:
+                    day = "Wednesday";
+                    break;
+                case 5:
+                    day = "Thursday";
+                    break;
+                case 6:
+                    day = "Friday";
+                    break;
+                case 7:
+                    day = "Saturday";
+                    break;
+                default:
+                    day = "Invalid value";
+                    break;
+            }
+            Console.WriteLine($"Day: {day}");*/
+            #endregion
+
+            #region Condicional Ternária
+            // (condicao) ? valor_se_verdadeiro : valor_se_falso
+            /*double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double desconto = (preco < 20) ? preco * 0.1 : preco * 0.05;
+
+            Console.WriteLine(desconto);*/
+            #endregion
+
+            #region Funções Strings
+            /*string original = "abcde FGHIJ ABC abc DEFG    ";
+            string s1 = original.ToUpper();
+            string s2 = original.ToLower();
+            string s3 = original.Trim(); // apaga os espaços
+
+            int n1 = original.IndexOf("bc");
+            int n2 = original.LastIndexOf("bc");
+
+            string s4 = original.Substring(3);
+            string s5 = original.Substring(3, 5);
+            string s6 = original.Replace('a', 'x'); //substitui o elemento da variavel
+            string s7 = original.Replace("abc", "xy");
+
+            bool b1 = String.IsNullOrEmpty(original); //Verifica se a variavel é nulo ou vazio
+            bool b2 = String.IsNullOrWhiteSpace(original); //Verifica se a variavel é nulo ou contem espacos em branco                        
+
+            Console.WriteLine("Original: -" + original + "-");
+            Console.WriteLine("ToUpper: -" + s1 + "-");
+            Console.WriteLine("ToLower: -" + s2 + "-");
+            Console.WriteLine("Trim: -" + s3 + "-");
+            Console.WriteLine("IndexOf('bc'): " + n1);
+            Console.WriteLine("LastIndexOf('bc'): " + n2);
+            Console.WriteLine("Substring(3): -" + s4 + "-");
+            Console.WriteLine("Substring(3, 5): -" + s5 + "-");
+            Console.WriteLine("Replace('a', 'x'): -" + s6 + "-");
+            Console.WriteLine("Replace('abc', 'xy'): -" + s7 + "-");
+            Console.WriteLine("IsNullOrEmpty: " + b1);
+            Console.WriteLine("IsNullOrWhiteSpace: " + b2);*/
+            #endregion
+
+            #region DateTime
+            /*DateTime d1 = DateTime.Now;
+             Console.WriteLine(d1);
+             Console.WriteLine(d1.Ticks);*/
+
+            /*DateTime d1 = new DateTime(2024, 12, 27);
+            DateTime d2 = new DateTime(2024, 12, 27, 03, 33, 15);
+            DateTime d3 = new DateTime(2024, 12, 27, 03, 33, 15, 500); //com millisegundos
+
+            DateTime d4 = DateTime.Now;
+            DateTime d5 = DateTime.Today;
+            DateTime d6 = DateTime.UtcNow;  //Pega o horario universal gmt
+
+            Console.WriteLine(d1);
+            Console.WriteLine(d2);
+            Console.WriteLine(d3);
+            Console.WriteLine(d4);
+            Console.WriteLine(d5);
+            Console.WriteLine(d6);
+            */
+
+            /*DateTime d1 = DateTime.Parse("2000-08-15");
+            DateTime d2 = DateTime.Parse("2000-08-15 13:05:58");
+
+            DateTime d3 = DateTime.Parse("15/08/2000");
+            DateTime d4 = DateTime.Parse("15/08/2000 13:05:58");
+
+            Console.WriteLine(d1);
+            Console.WriteLine(d2);
+            Console.WriteLine(d3);
+            Console.WriteLine(d4);*/
+
+            //Mascaras de formatação no datetime
+            /*DateTime d1 = DateTime.ParseExact("2000-08-15", "yyyy-MM-dd", CultureInfo.InvariantCulture);
+            DateTime d2 = DateTime.ParseExact("15/08/2000 13:05:58", "dd/MM/yyyy HH:mm:ss", CultureInfo.InvariantCulture);
+
+            Console.WriteLine(d1);
+            Console.WriteLine(d2);*/
+            #endregion
+
+            #region TimeSpan
+            /*TimeSpan t1 = new TimeSpan(0, 1, 30);
+
+            Console.WriteLine(t1);
+            Console.WriteLine(t1.Ticks);*/
+
+            /*TimeSpan t1 = new TimeSpan();
+            TimeSpan t2 = new TimeSpan(900000000L);
+            TimeSpan t3 = new TimeSpan(2, 11, 21);
+            TimeSpan t4 = new TimeSpan(1, 10, 20, 5);
+            TimeSpan t5 = new TimeSpan(5, 2, 21, 59, 321);
+
+            Console.WriteLine(t1);
+            Console.WriteLine(t2);
+            Console.WriteLine(t3);
+            Console.WriteLine(t4);
+            Console.WriteLine(t5);*/
+
+            /*TimeSpan t1 = TimeSpan.FromDays(1.5);
+            TimeSpan t2 = TimeSpan.FromHours(1.5);
+            TimeSpan t3 = TimeSpan.FromMinutes(1.5);
+            TimeSpan t4 = TimeSpan.FromSeconds(1.5);
+            TimeSpan t5 = TimeSpan.FromMilliseconds(1.5);
+            TimeSpan t6 = TimeSpan.FromTicks(900000000L);
+
+            Console.WriteLine(t1);
+            Console.WriteLine(t2);
+            Console.WriteLine(t3);
+            Console.WriteLine(t4);
+            Console.WriteLine(t5);
+            Console.WriteLine(t6);*/
+            #endregion
+
+            #region Propriedades e Operaçõs com DateTime
+            DateTime d = new DateTime(2001, 8, 15, 13, 45, 58, 275);
+
+            Console.WriteLine(d);
+            Console.WriteLine("1) Date: " + d.Date);
+            Console.WriteLine("2) Day: " + d.Day);
+            Console.WriteLine("3) DayOfWeek: " + d.DayOfWeek);
+            Console.WriteLine("4) DayOfYear: " + d.DayOfYear);
+            Console.WriteLine("5) Hour: " + d.Hour);
+            Console.WriteLine("6) Kind: " + d.Kind);
+            Console.WriteLine("7) Millisecond: " + d.Millisecond);
+            Console.WriteLine("8) Minute: " + d.Minute);
+            Console.WriteLine("9) Month: " + d.Month);
+            Console.WriteLine("10) Second: " + d.Second);
+            Console.WriteLine("11) Ticks: " + d.Ticks);
+            Console.WriteLine("12) TimeOfDay: " + d.TimeOfDay);
+            Console.WriteLine("13) Year: " + d.Year);
+
+            //minuto 2
+            #endregion
+        }
+    }
+}
