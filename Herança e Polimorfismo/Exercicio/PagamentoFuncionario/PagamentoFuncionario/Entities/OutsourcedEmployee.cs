447f36d5b42f85064f39d73f065d854dd7173d21
GilBarb0sa <gilbarbosafilho@gmail.com>
2024-11-06 12:58:41 -0300

    desafio heranca e polimorfismo 1

---
+++ Herança e Polimorfismo/Exercicio/PagamentoFuncionario/PagamentoFuncionario/Entities/OutsourcedEmployee.cs
@@ -0,0 +1,23 @@
+﻿namespace PagamentoFuncionario.Entities
+{
+    class OutsourcedEmployee : Employee
+    {
+        public double AdditionalCharge { get; set; }
+
+        public OutsourcedEmployee()
+        {
+        }
+
+        public OutsourcedEmployee(string name, int hours, double valuePerHour, double additionalCharge) 
+            : base(name, hours, valuePerHour)  //Aproveita todos os argumentos de employee
+        {
+            AdditionalCharge = additionalCharge;
+        }
+
+        public override double Payment()
+        {
+            return base.Payment() + 1.1 * AdditionalCharge;
+
+        }
+    }
+}
