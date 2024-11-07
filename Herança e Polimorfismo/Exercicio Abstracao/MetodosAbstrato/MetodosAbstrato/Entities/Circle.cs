71877f1736c9518fc5087f783c15647e3321810c
GilBarb0sa <gilbarbosafilho@gmail.com>
2024-11-07 13:04:56 -0300

    metodos abstratos

---
+++ Herança e Polimorfismo/Exercicio Abstracao/MetodosAbstrato/MetodosAbstrato/Entities/Circle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using MetodosAbstrato.Entities.Enums;
+
+namespace MetodosAbstrato.Entities
+{
+    class Circle : Shape
+    {
+        public double Radius { get; set; }
+
+        public Circle(double radius, Color color) : base (color)
+        {
+            Radius = radius;
+        }
+
+        public override double Area()
+        {
+            return Math.PI * Radius * Radius;
+        }
+    }
+}
---
+++ Herança e Polimorfismo/Exercicio Abstracao/MetodosAbstrato/MetodosAbstrato/Entities/Rectangle.cs
@@ -0,0 +1,22 @@
+﻿using MetodosAbstrato.Entities.Enums;
+
+namespace MetodosAbstrato.Entities
+{
+    class Rectangle : Shape
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+
+        public Rectangle(double width, double height, Color color) : base(color)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public override double Area() 
+        //Para implementar uma classe ou metodo abstrato, tem que ser override e construir o metodo aqui tbm
+        {
+            return Width * Height;
+        }
+    }
+}
