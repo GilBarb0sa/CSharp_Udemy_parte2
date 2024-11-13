4b852f8f212ec5a15b3772b899377e2675135f4b
GilBarb0sa <gilbarbosafilho@gmail.com>
2024-11-13 12:07:55 -0300

    estudos de try catch

---
+++ Tratamento de Excessoes/EstruturaTryCatch/EstruturaTryCatch/Entities/Reservation.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace EstruturaTryCatch.Entities
+{
+    class Reservation
+    {
+        public int RoomNumber { get; set; }
+        public DateTime CheckIn { get; set; }
+        public DateTime CheckOut { get; set; }
+
+        public Reservation()
+        {
+        }
+
+        public Reservation(int roomNumber, DateTime checkIn, DateTime checkOut)
+        {
+            RoomNumber = roomNumber;
+            CheckIn = checkIn;
+            CheckOut = checkOut;
+        }
+
+        public int Duration()
+        {
+            TimeSpan duration = CheckOut.Subtract(CheckIn);  //pega a diferença de um instante e outro
+            return (int)duration.TotalDays;  //TotalDays é um double, ai faz um casting para retornar um inteiro.
+        }
+
+        public void UpdateDates(DateTime checkIn, DateTime chekOut)
+        {
+            CheckIn = checkIn;
+            CheckOut = chekOut;
+        }
+
+        public override string ToString()
+        {
+            return "Room "
+                + RoomNumber
+                + ", check-in: "
+                + CheckIn.ToString("dd/MM/yyyy")
+                + ", check-out: "
+                + CheckOut.ToString("dd/MM/yyyy")
+                + ", "
+                + Duration()
+                + " nights";
+        }
+    }
+}
---
+++ Tratamento de Excessoes/EstruturaTryCatch/EstruturaTryCatch/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.IO;
+
+using System;
+using EstruturaTryCatch.Entities;
+
+namespace EstruturaTryCatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Teste do Finally para abrir arquivo
+            //FileStream fs = null;
+            //Uso do try, catch e finally para testar a abertura de um arquivo
+            //try
+            //{
+            //    fs = new FileStream(@"C:\temp\data.txt", FileMode.Open);
+            //    StreamReader sr = new StreamReader(fs);
+            //    string line = sr.ReadLine();
+            //    Console.WriteLine(line);
+            //}
+            //catch (FileNotFoundException e)
+            //{
+            //    Console.WriteLine(e.Message);               
+            //}
+            //finally
+            //{
+            //    if (fs != null)
+            //    {
+            //        fs.Close();
+            //    }
+            //}
+            #endregion
+             
+            Console.Write("Room Number: ");
+            int number = int.Parse(Console.ReadLine());
+            Console.Write("Check-in date (dd/MM/yyyy): ");
+            DateTime checkIn = DateTime.Parse(Console.ReadLine());
+            Console.Write("Check-out date (dd/MM/yyyy): ");
+            DateTime checkOut = DateTime.Parse(Console.ReadLine());
+
+            if (checkOut <= checkIn)
+            {
+                Console.WriteLine("Error in reservation: Check-out dat must be after check-in date");
+            }
+            else
+            {
+                Reservation reservation = new Reservation(number, checkIn, checkOut);
+                Console.WriteLine("Reservation: " + reservation);
+
+                Console.WriteLine();
+                Console.WriteLine("Enter data to update the reservation: ");
+                Console.Write("Check-in date (dd/MM/yyyy): ");
+                checkIn = DateTime.Parse(Console.ReadLine());
+                Console.Write("Check-out date (dd/MM/yyyy): ");
+                checkOut = DateTime.Parse(Console.ReadLine());
+
+                DateTime now = DateTime.Now;
+                if (checkIn < now || checkOut < now)
+                {
+                    Console.WriteLine("Error in reservation: Reservation dates for update must be future dates");
+                }
+                else if (checkOut <= checkIn)
+                {
+                    Console.WriteLine("Error in reservation: Check-ou date must be after check-in dates");
+                }
+                else
+                {
+                    reservation.UpdateDates(checkIn, checkOut);
+                    Console.WriteLine("Reservation: " + reservation );
+                }
+            }
+        }
+    }
+}
