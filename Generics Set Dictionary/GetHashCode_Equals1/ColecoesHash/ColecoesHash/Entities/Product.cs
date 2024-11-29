b0853d9a032bebaf2f33e50acd631dd324eaa244
GilBarb0sa <gilbarbosafilho@gmail.com>
2024-11-29 12:13:41 -0300

    colecoes hash

---
+++ Generics Set Dictionary/GetHashCode_Equals1/ColecoesHash/ColecoesHash/Entities/Product.cs
@@ -0,0 +1,29 @@
+﻿namespace ColecoesHash.Entities
+{
+    class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public override int GetHashCode()
+        {
+            return Name.GetHashCode() + Price.GetHashCode(); // metodo para verificar se ambos os itens são iguais, o gethashcode gera um numero aleatorio para cada objeto
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Product))
+            {
+                return false;
+            }
+            Product other = obj as Product;
+            return Name.Equals(other.Name) && Price.Equals(other.Price);
+        }
+    }
+}
---
+++ Generics Set Dictionary/GetHashCode_Equals1/ColecoesHash/ColecoesHash/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using ColecoesHash.Entities;
+
+namespace ColecoesHash
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<Product> a = new HashSet<Product>();
+            a.Add(new Product("Tv", 900.00));
+            a.Add(new Product("Notebook", 1200.0));
+
+            HashSet<Point> b = new HashSet<Point>();
+            b.Add(new Point(3, 4));
+            b.Add(new Point(5, 10));
+
+            Product prod = new Product("Notebook", 1200.0);
+            Console.WriteLine(a.Contains(prod));
+
+            Point p = new Point(5, 10); //Quando o tipo é struct, vai comparar por conteudo e não por referencia
+            Console.WriteLine(b.Contains(p));
+        }
+    }
+}
