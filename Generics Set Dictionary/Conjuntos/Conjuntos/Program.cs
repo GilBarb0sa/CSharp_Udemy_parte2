55cdb25b355c542421e11b6ccc04fe5d2a138401
Gilberto Barbosa da Silva Filho <gilbarbosafilho@gmail.com>
2024-12-09 16:12:24 -0300

    sobre conjuntos

---
+++ Generics Set Dictionary/Conjuntos/Conjuntos/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Conjuntos.Entities;
+
+namespace Conjuntos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Logica para le arquivo de texto
+
+            HashSet<LogRecord> set = new HashSet<LogRecord>();
+
+            Console.Write("Enter file full path: ");
+            string path = Console.ReadLine();
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream) {
+                        string[] line = sr.ReadLine().Split(' ');
+                        string name = line[0];
+                        DateTime instant = DateTime.Parse(line[1]);
+                        set.Add(new LogRecord { UserName = name, Instant = instant });                        
+                    }
+                    Console.WriteLine("Total user: " + set.Count);
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
